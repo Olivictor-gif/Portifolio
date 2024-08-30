6570a28aa85c0399c9744024d55d4560f6169eba
Olivictor-gif <joaovictor_bs@outlook.com>
2024-08-29 22:13:34 -0300

    att2

--- portifolio-vite/src/App.jsx
+++ portifolio-vite/src/App.jsx
@@ -16,14 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Portifólio em construção</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button href="https://www.linkedin.com/in/joão-victor-oliveira-479b471a1/">
+          Linkedin
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button href="github.com/Olivictor-gif">
+          Github
+        </button>
+       
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
