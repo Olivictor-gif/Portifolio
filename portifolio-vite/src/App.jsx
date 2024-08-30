476b24bb814799fdd6aa52dec209746a8e980748
Olivictor-gif <joaovictor_bs@outlook.com>
2024-08-29 22:19:08 -0300

    aa

--- portifolio-vite/src/App.jsx
+++ portifolio-vite/src/App.jsx
@@ -9,9 +9,14 @@ function App() {
      
       <h1>Portifólio em construção</h1>
       <div className="card">
-        <button href="https://www.linkedin.com/in/joão-victor-oliveira-479b471a1/">
+        <a href="https://www.linkedin.com/in/joão-victor-oliveira-479b471a1/"><button >
           Linkedin
         </button>
+        </a>
+        <a href="https://www.github.com/Olivictor-gif"><button >
+          Github
+        </button>
+        </a>
         <button href="github.com/Olivictor-gif">
           Github
         </button>
