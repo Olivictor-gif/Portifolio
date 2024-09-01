5953da78b101233b32358b989977a6c667277582
Olivictor-gif <joaovictor_bs@outlook.com>
2024-08-31 21:04:07 -0300

    alteração

--- portifolio-vite/src/App.jsx
+++ portifolio-vite/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-    <>
+   /* <>
      
       <h1>Portifólio em construção</h1>
       <div className="card">
@@ -19,7 +19,25 @@ function App() {
         </a>
       </div>
       
-    </>
+   </>
+   */
+  <>
+   <div class="container">
+   <div class="profile">
+       <img src="sua_foto.jpg" alt="Foto de João Victor">
+   </div>
+   <div class="info">
+       <h1>João Victor</h1>
+       <h2>Front end Developer</h2>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, nisl ut tempor bibendum, dolor mauris posuere nunc, et varius sapien nunc non lacus. Quisque sit amet lacinia arcu.</p>
+       <div class="buttons">
+           <a href="https://github.com/seu_usuario" target="_blank" class="button">GitHub</a>
+           <a href="https://www.linkedin.com/in/seu_usuario" target="_blank" class="button">LinkedIn</a>
+           <a href="seu_curriculo.pdf" target="_blank" class="button">Currículo</a>
+       </div>
+   </div>
+</div>
+</>
   )
 }
 
